9d77d787a21953c2014a67431dc4e12cf1c7ab12
larrybloom <aniediabasi.joseph@decagon.dev>
2024-03-02 18:48:30 +0100

    cfh

--- Models/DTOs/AuthorStatsDto.cs
+++ Models/DTOs/AuthorStatsDto.cs
@@ -5,7 +5,7 @@
         public string AuthorId { get; set; }
         public int TotalNumOfArticles { get; set; }
         public int TotalReportedArticles { get; set; }
-      
+
     }
 
     public class AuthorsStatsDto
--- Models/DTOs/CommentDto.cs
+++ Models/DTOs/CommentDto.cs
@@ -7,5 +7,5 @@ public class CommentDto
     public string? Image { get; set; }
     public string Text { get; set; }
     public DateTime CreatedOn { get; set; }
-   
+
 }
